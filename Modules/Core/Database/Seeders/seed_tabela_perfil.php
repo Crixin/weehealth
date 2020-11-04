7152027de34246887022880fef97c26e095131cb
Bruno Colla <bruno.colla@weecode.com.br>
2020-11-04 11:40:19 -0300

    seed modulo core

---
+++ Modules/Core/Database/Seeders/seed_tabela_perfil.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Repositories\PerfilRepository;
+
+class PerfilTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $perfilRepository = new PerfilRepository();
+
+        $perfilRepository->create([
+            'id' => 1,
+            'nome' => 'Weecode'
+        ]);
+    }
+}
