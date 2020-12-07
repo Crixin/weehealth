f506a3332b13f6397523b8a30108dc3ecbbed630
Bruno Colla <bruno.colla@weecode.com.br>
2020-12-07 09:45:27 -0300

    teste migratiosn

--- Modules/Docs/Database/Migrations/2020_11_24_164520_create_docs_documento_externo.php
+++ Modules/Docs/Database/Migrations/2020_11_24_164520_create_docs_documento_externo.php
@@ -27,7 +27,6 @@ class CreateDocsDocumentoExterno extends Migration
             $table->foreign('user_id')->references('id')->on('core_users');
             $table->integer('grupo_id')->unsigned();
             $table->foreign('grupo_id')->references('id')->on('core_grupo');
-            $table->integer('empresa_id')->nullable(true);
             $table->foreignId('empresa_id')->constrained('core_empresa');
             $table->string('revisao')->nullable(true);
             $table->date('validade')->nullable(true);
